d5e86874ffcd7a0728ce7fe60948551a4a85337f
hitesh-webRepo <webdev.hitesh@gmail.com>
2022-05-12 22:35:04 +0530

    new changes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Stichoza\GoogleTranslate\GoogleTranslate;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
